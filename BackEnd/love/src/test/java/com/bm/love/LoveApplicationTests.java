6c3d3c984441f9318030aaf17330cac516176101
JHB <jo7hb@naver.com>
2023-05-05 16:59:05 +0900

    Initial commit

---
+++ BackEnd/love/src/test/java/com/bm/love/LoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
