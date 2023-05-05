adc53e94c341a75b19b5f1e9dbde765acaccc776
JHB <jo7hb@naver.com>
2023-05-06 04:22:10 +0900

    init project

--- BackEnd/src/main/java/com/bm/love/controller/HelloController.java
+++ BackEnd/src/main/java/com/bm/love/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.bm.love.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api")
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
