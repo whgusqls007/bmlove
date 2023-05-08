a02e7cb47743617a98cec03c5882c1bed2bb554f
JHB <jo7hb@naver.com>
2023-05-08 23:52:08 +0900

    Test Image Post and Get

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://18.224.45.108/api/",
+    baseURL: "http://18.191.103.58/api/",
     // baseURL: "http://127.0.0.1:8080/",
     headers: {
         "Content-Type": "Application/json",
