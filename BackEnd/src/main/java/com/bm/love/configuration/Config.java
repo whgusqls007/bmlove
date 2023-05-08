4e6e6a53ca19ae7dc1a196f49733456c9a297571
JHB <jo7hb@naver.com>
2023-05-08 22:01:25 +0900

    Test Image Post and Get

--- BackEnd/src/main/java/com/bm/love/configuration/Config.java
+++ BackEnd/src/main/java/com/bm/love/configuration/Config.java
@@ -1,13 +1,27 @@
 package com.bm.love.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
+    @Value("${resources.location}")
+    private String resourcesLocation;
+    @Value("${resources.uri_path:}")
+    private String resourcesUriPath;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcesUriPath + "/**")
+                .addResourceLocations("file://" + resourcesLocation);
+
     }
 }
