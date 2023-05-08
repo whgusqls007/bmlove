4577ca54ca0ac811dc0112475800ee9f6f3bee2f
JHB <jo7hb@naver.com>
2023-05-08 21:12:25 +0900

    Test Image Post and Get

--- BackEnd/src/main/java/com/bm/love/controller/PlaceController.java
+++ BackEnd/src/main/java/com/bm/love/controller/PlaceController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
