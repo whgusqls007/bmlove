cfc7268881e447190da96d530a817287b908f11c
JHB <jo7hb@naver.com>
2023-05-09 04:21:48 +0900

    Test Image Post and Get

--- imageserver/src/main/java/com/bmlove/imageserver/configuration/Config.java
+++ imageserver/src/main/java/com/bmlove/imageserver/configuration/Config.java
@@ -1,26 +1,13 @@
 package com.bmlove.imageserver.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config implements WebMvcConfigurer {
-
-    // @Value("${image.upload.path}")
-    // private String uploadPath;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
     }
-
-    // @Override
-    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    // registry.addResourceHandler("/images/**")
-    // .addResourceLocations("file://" +
-    // "/var/jenkins_home/workspace/bmlove/imageserver/");
-    // }
 }
