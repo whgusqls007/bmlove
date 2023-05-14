e44e8cb0b09e553b75691913346a1980213a4f29
JHB <jo7hb@naver.com>
2023-05-14 21:57:57 +0900

    featuer : Set swagger Set swagger to make API Docs for BMLove Add swagger configuration into Config.java public Docket api() private ApiInfo apiInfo()

--- BackEnd/src/main/java/com/bm/love/configuration/Config.java
+++ BackEnd/src/main/java/com/bm/love/configuration/Config.java
@@ -1,13 +1,44 @@
 package com.bm.love.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
 @Configuration
 public class Config implements WebMvcConfigurer {
+    // Cors Config
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
     }
+    // Cors Config End
+
+    // Swagger Config
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bm.love.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Bin Min Love")
+                .description("Hyunbin loves Sumin Api Docs")
+                .version("1.0")
+                .build();
+    }
+    // Swagger Config End
 }
